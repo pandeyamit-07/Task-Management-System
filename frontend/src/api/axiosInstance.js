0cb22b0f3ba7dc94d070bf8f991425cead29ff46
Amit Pandey <154595576+pandeyamit-07@users.noreply.github.com>
2025-11-06 08:07:15 +0530

    Update axiosInstance.js

--- frontend/src/api/axiosInstance.js
+++ frontend/src/api/axiosInstance.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const instance = axios.create({
-  baseURL:https://task-management-system-lryo.onrender.com || "http://localhost:5000",
+  baseURL:import.meta.env.VITE_API_URL || "http://localhost:5000",
 });
 
 export default instance;
